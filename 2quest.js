9d89486aeba3890e73eb833b3d20f3c6f1680644
Vlad <vlad.privet@mil.ru>
2024-10-29 16:40:16 +0300

    2 quest

---
+++ 2quest.js
@@ -0,0 +1,63 @@
+"use strict"
+
+// Условия
+
+// if (4 == 9) {
+//     console.log('ok!');
+// } else {
+//     console.log('no!');
+// }
+
+//////////////////////////////////////////////////////////////
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log("Много")
+// } else {
+//     console.log("ok!");
+// }
+
+// Тернарый оператор (участвует 3 аргумента)
+
+// (num === 50) ? console.log("Ok!") : console.log("Error");
+
+//////////////////////////////////////////////////////////////
+
+// const num = 50;
+
+// switch (num) {
+//     case 49:
+//         console.log("error");
+//         break;
+//     case 50:
+//         console.log("Great!");
+//         break;
+//     default:
+//         console.log("no it's time");
+//         break;
+// }
+
+//////////////////////////////////////////////////////////////
+
+
+// const hamburger = true;
+// const fries = true;
+
+// if (hamburger && fries ) {
+//     console.log("ya daun");
+// }
+
+// console.log((hamburger && fries));
+
+const hamburger = 3;
+const fries = 1;
+const cola = 0;
+
+if (hamburger === 3 && cola === 1 && fries) {
+    console.log("Nice!");
+} else {
+    console.log("good bye!");
+}
