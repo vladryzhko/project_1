0ee7c13eb961d2ec14eef09d3eb924ca482e4d7d
Vlad <vlad.privet@mil.ru>
2024-10-30 17:31:26 +0300

    next 2 quest

--- 2quest.js
+++ 2quest.js
@@ -52,12 +52,49 @@
 
 // console.log((hamburger && fries));
 
-const hamburger = 3;
-const fries = 1;
-const cola = 0;
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 0;
 
-if (hamburger === 3 && cola === 1 && fries) {
-    console.log("Nice!");
-} else {
-    console.log("good bye!");
-}
+// if (hamburger === 3 && cola === 1 && fries) {
+//     console.log("Nice!");
+// } else {
+//     console.log("good bye!");
+// }
+
+//////////////////////////////////////////////////////////////
+
+// console.log( NaN || 2 || undefined );
+// console.log( NaN && 2 && undefined );
+// console.log( 1 && 2 && 3 );
+// console.log( !1 && 2 || !3 );
+// console.log( 25 || null && !3 );
+// console.log( NaN || null || !3 || undefined || 5);
+// console.log( NaN || null && !3 && undefined || 5);
+// console.log( 5 === 5 && 3 > 1 || 5);
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+// if (hamburger || cola || fries === 3 || nuggets) {
+//    console.log('Done!')
+// }
+// console.log(hamburger || cola || fries === 3 || nuggets);
+
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+
+// if (hamburger && cola || fries === 3 && nuggets) {
+//    console.log('Done!')
+// }
+
+// console.log(hamburger && cola || fries === 3 && nuggets);
+
+//////////////////////////////////////////////////////////////
+
+
