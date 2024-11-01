12789849ff18f564112438d3c8928c994f8939ce
Vlad <vlad.privet@mil.ru>
2024-11-01 17:44:30 +0300

    third commit

--- 1quest.js
+++ 1quest.js
@@ -21,24 +21,24 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
-'use strict';
+// 'use strict';
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+// const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-const a = prompt("один из просмотренных фильмов?", ""),
-      b = prompt("На сколько оцените его?", ""),
-      c = prompt("один из просмотренных фильмов?", ""),
-      d = prompt("На сколько оцените его?", "");
+// const a = prompt("один из просмотренных фильмов?", ""),
+//       b = prompt("На сколько оцените его?", ""),
+//       c = prompt("один из просмотренных фильмов?", ""),
+//       d = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
--- 2quest.js
+++ 2quest.js
@@ -97,4 +97,85 @@
 
 //////////////////////////////////////////////////////////////
 
+//                            ЦИКЛ
 
+// let num = 50;
+
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
+
+// for (let i = 1; i < 10; i++) {
+//     if (i === 6) {
+//         // break;
+//         continue;
+//     }
+    
+//     console.log(i);
+// }
+
+//////////////////////////////////////////////////////////////
+
+// for (let i = 0; i < 3; i++) {
+//     console.log(i);
+//     for (let j = 0; j < 3; j++) {
+//         console.log(j);
+//     }
+// }
+
+//////////////////////////////////////////////////////////////
+
+// let result = "";
+// const length = 7;
+
+// for (let i = 1; i < length; i++) {
+
+//     for (let j = 0; j < i; j++) {
+//         result += "*";
+//     }
+    
+//     result += '\n'
+// }
+
+
+// console.log(result);
+
+//////////////////////////////////////////////////////////////
+
+
+// first: for (let i = 0; i < 3; i++) {
+//     console.log(`First level: ${i}`);
+//     for (let j = 0; j < 3; j++) {
+//         console.log(`Second level: ${j}`);
+//         for (let k = 0; k < 3; k++) {
+//             if (k === 2) continue first
+//             console.log(`Third level: ${k}`);
+//         }
+//     } 
+// }
+
+//////////////////////////////////////////////////////////////
+
+// function thirdTask() {
+//     for (let i = 2; 2 <= 10; i++) {
+//         if (i % 2 === 0) {
+//             console.log(i);
+//         }
+//     }
+// }
+
+//////////////////////////////////////////////////////////////
+let num = 3;
+
+if (num % 3 === 0) {
+    console.log('da');
+} else {
+    console.log("net");
+}
