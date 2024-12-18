bb54c53bf0152069023cdcd28099a96374a83cba
Vlad <vlad.privet@mil.ru>
2024-12-18 14:59:58 +0300

    new update

--- js/19codewars.js
+++ js/19codewars.js
@@ -38,6 +38,7 @@ function DNAtoRNA(dnk) {
 
 console.log(DNAtoRNA('ATCG'));
 
+///////////////////////////////////////////////////
 
 function findAverage(array) {
     
@@ -54,3 +55,88 @@ function findAverage(array) {
 }
 
 console.log(findAverage([2, 3, 4, 5, 6, 7]));
+
+////////////////////////////////////////////////////////
+
+function abbrevName(name){
+    return name.split(" ").map((part) => part[0].toUpperCase()).join(".");
+}
+
+console.log(abbrevName("Vlad Ryzhko"));
+
+
+////////////////////////////////////////////////////////
+
+
+function choseYourNumber(number) {
+    return number % 2 === 0 ? "Even" : "Odd";
+}
+
+console.log(choseYourNumber(21));
+
+///////////////////////////////////////////////////////
+
+let nameStretch = {
+    a: 1,
+    b: 2
+ };
+ 
+ let target = Object.assign({}, nameStretch);
+ 
+ console.log(target);
+ 
+ ///////////////////////////////////////////////////////
+ 
+ 
+ let obj = {
+     a: "lox",
+     b: "pidr",
+     c: "petuh",
+     d: {
+         f: "daun"
+     }
+   };
+ 
+ obj.d.f = "nice";
+ let objCopy = Object.assign({}, obj);
+ 
+ 
+ console.log(obj);
+ console.log(objCopy);
+ 
+ ///////////////////////////////////////////////////////
+ 
+ 
+ let obj = {
+     name: 'scotch.io',
+     exec: function exec() {
+       return true;
+     },
+   }
+   
+ let method1 = Object.assign({}, obj);
+ let method2 = JSON.parse(JSON.stringify(obj));
+ 
+ console.log(method1);
+ console.log(method2);
+ 
+ ///////////////////////////////////////////////////////
+ 
+ 
+ let obj = { 
+     a: 'a',
+     b: { 
+       c: 'c',
+       d: 'd',
+     },
+   }
+   
+   obj.c = obj.b;
+   obj.e = obj.a;
+   obj.b.c = obj.c;
+   obj.b.d = obj.b;
+   obj.b.e = obj.b.c;
+   
+   let newObj = Object.assign({}, obj);
+   
+   console.log(newObj);
---
+++ js/21workWIthDate.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const now = new Date("2020-05-01");
+// new Date.parse("2020-05-01");
+
+// console.log(now.setHours(20));
+console.log(now);
+
+
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getHours());
+// console.log(now.getUTCHours());
+
+// console.log(now.getTimezoneOffset());
+// console.log(now.getTime());
